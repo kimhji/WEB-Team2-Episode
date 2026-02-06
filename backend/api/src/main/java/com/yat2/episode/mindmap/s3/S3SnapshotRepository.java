3e0f4f1d9e434d8a68c7f5571ee939e41725b138
kimhji <twin4185@naver.com>
2026-02-06 13:48:36 +0900

    S3 Repository 되던걸로

--- backend/api/src/main/java/com/yat2/episode/mindmap/s3/S3SnapshotRepository.java
+++ backend/api/src/main/java/com/yat2/episode/mindmap/s3/S3SnapshotRepository.java
@@ -1,33 +1,30 @@
 package com.yat2.episode.mindmap.s3;
 
-import lombok.extern.slf4j.Slf4j;
+import com.yat2.episode.mindmap.s3.dto.S3UploadResponseDto;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
-import com.yat2.episode.global.exception.CustomException;
-import com.yat2.episode.global.exception.ErrorCode;
-import com.yat2.episode.mindmap.s3.dto.S3UploadResponseDto;
-
-@Slf4j
 @Repository
 public class S3SnapshotRepository {
     private final S3PostSigner s3PostSigner;
     private final AwsCredentialsProvider credentialsProvider;
+    private final String bucketName;
+    private final String region;
+    private final String endpoint;
 
-    public S3SnapshotRepository(S3PostSigner s3PostSigner, AwsCredentialsProvider credentialsProvider) {
+    public S3SnapshotRepository(S3PostSigner s3PostSigner, AwsCredentialsProvider credentialsProvider,
+                                S3Properties s3Properties) {
         this.s3PostSigner = s3PostSigner;
         this.credentialsProvider = credentialsProvider;
+        this.bucketName = s3Properties.getBucket().getName();
+        this.region = s3Properties.getRegion();
+        this.endpoint = s3Properties.getEndpoint();
     }
 
     public S3UploadResponseDto createPresignedUploadInfo(String objectKey) {
-        try {
-            AwsCredentials credentials = credentialsProvider.resolveCredentials();
+        AwsCredentials credentials = credentialsProvider.resolveCredentials();
 
-            return s3PostSigner.generatePostFields(objectKey, credentials);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            throw new CustomException(ErrorCode.S3_URL_FAIL);
-        }
+        return s3PostSigner.generatePostFields(bucketName, objectKey, region, endpoint, credentials);
     }
 }
